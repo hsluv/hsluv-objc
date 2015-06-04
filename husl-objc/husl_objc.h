048c3e06aec2d58f4aa15d9f6cdbe521b4da0f66
Roger <roger@macmini.local>
2015-06-04 15:47:28 +0200

    Initial Commit

---
+++ husl-objc/husl_objc.h
@@ -0,0 +1,13 @@
+//
+//  husl_objc.h
+//  husl-objc
+//
+//  Created by Roger on 4/6/15.
+//  Copyright (c) 2015 Roger Tallada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface husl_objc : NSObject
+
+@end
---
+++ husl-objc/husl_objc.m
@@ -0,0 +1,13 @@
+//
+//  husl_objc.m
+//  husl-objc
+//
+//  Created by Roger on 4/6/15.
+//  Copyright (c) 2015 Roger Tallada. All rights reserved.
+//
+
+#import "husl_objc.h"
+
+@implementation husl_objc
+
+@end
