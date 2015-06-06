8f780ba9476f6107f2db7ea879cc1a991dd645ab
Roger <roger@macmini.local>
2015-06-06 14:59:41 +0200

    Adds header function descriptions.

--- husl-objc/husl_objc.h
+++ husl-objc/husl_objc.h
@@ -18,10 +18,16 @@ NSString *rgbToHex(CGFloat red, CGFloat green, CGFloat blue);
 // Accepts an hex color, as in "#012C4A", and stores its red, green and blue components with values between 0 and 1.
 BOOL hexToRgb(NSString *hex, CGFloat *red, CGFloat *green, CGFloat *blue);
 
-
+// Hue is a value between 0 and 360, saturation and lightness between 0 and 100. Stores the RGB in values between 0 and 1.
 void huslToRgb(CGFloat hue, CGFloat saturation, CGFloat lightness, CGFloat *red, CGFloat *green, CGFloat *blue);
+
+// Red, green and blue values between 0 and 1, stores the husl components with hue between 0 and 360, saturation and lightness between 0 and 100.
 void rgbToHusl(CGFloat red, CGFloat green, CGFloat blue, CGFloat *hue, CGFloat *saturation, CGFloat *lightness);
+
+// Hue is a value between 0 and 360, saturation and lightness between 0 and 100. Stores the RGB in values between 0 and 1.
 void huslpToRgb(CGFloat hue, CGFloat saturation, CGFloat lightness, CGFloat *red, CGFloat *green, CGFloat *blue);
+
+// Red, green and blue values between 0 and 1, stores the huslp components with hue between 0 and 360, saturation and lightness between 0 and 100.
 void rgbToHuslp(CGFloat red, CGFloat green, CGFloat blue, CGFloat *hue, CGFloat *saturation, CGFloat *lightness);
 
 #endif
